55053e559a6099175906f6fd34b09f7edaa20d7e
Anya Snow <emailasnow@gmail.com>
2019-02-18 01:24:57 -0800

    add script file

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-//  <script>
-//         document.addEventListener("touchstart", function() {},false);
-//     </script>
+
+
+        document.addEventListener("touchstart", function() {},false);
+    
